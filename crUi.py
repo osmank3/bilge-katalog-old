03be1aa341dcaebc9c91a8b0d3bb2fc322e2373f
osmank3 <osmank3@gmail.com>
2011-01-28 01:47:21 +0200

    yeni geliştirme dizin yapısına geçildi.

--- crUi.py
+++ crUi.py
@@ -8,15 +8,15 @@ import sys
 reload(sys).setdefaultencoding("utf-8")
 
 if "create" in sys.argv:
-    liste = os.listdir("uiQt")
+    liste = os.listdir("qt/ui")
     for i in liste:
         if i[-2:] == "ui":
-            os.system("pyuic4 uiQt/%s -o uiQt_%s.py"% (i, i[:-3]))
+            os.system("pyuic4 qt/ui/%s -o qt/uiQt_%s.py"% (i, i[:-3]))
         elif i[-3:] == "qrc":
-            os.system("pyrcc4 uiQt/%s -o %s_rc.py"% (i, i[:-4]))
+            os.system("pyrcc4 qt/ui/%s -o qt/%s_rc.py"% (i, i[:-4]))
         
 elif "delete" in sys.argv:
-    liste = os.listdir("./")
+    liste = os.listdir("qt")
     for i in liste:
         if i.find("uiQt_") != -1 or i.find("_rc.py") != -1:
-            os.remove(i)
+            os.remove("qt/" + i)
